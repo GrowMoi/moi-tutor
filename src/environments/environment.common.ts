2b04363f45742d8fae69c97de238c58f7e898a81
Jhymer Martínez <jhmartinez1991@gmail.com>
2020-02-19 11:09:29 -0500

    Configure environments

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,19 @@
+export const common = {
+  ACCESS_TOKEN_KEY: 'moiTutorAccessToken',
+  CLIENT_KEY: 'moiTutorClient',
+  EXPIRY_KEY: 'moiTutorExpiry',
+  TOKEN_TYPE_KEY: 'moiTutorTokenType',
+  UID_KEY: 'moiTutorUid',
+  USER_INFO: 'userInfo',
+  ACCESS_HEADERS: {
+    accessToken: 'access-token',
+    client: 'client',
+    expiry: 'expiry',
+    tokenType: 'token-type',
+    uid: 'uid'
+  },
+  roles: {
+    ADMIN: 'admin',
+    TUTOR: 'tutor'
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,13 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { common } from './environment.common';
 
 export const environment = {
-  production: false
+  production: false,
+  PRODUCTS_URL: '/#',
+  API_URL: 'http://localhost:5000',
+  ...common
 };
 
 /*
