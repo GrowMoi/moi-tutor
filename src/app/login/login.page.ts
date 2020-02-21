a2fc643d45a4a40f6b50aae7a4d2408fb25d2135
Jhymer Martínez <jhmartinez1991@gmail.com>
2020-02-20 22:52:29 -0500

    Show loading when login user

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { AuthenticateService } from '../services/authenticate.service';
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController } from '@ionic/angular';
 import { LOGIN } from '../actions';
-import { NgRedux } from '@angular-redux/store';
+import { NgRedux, select } from '@angular-redux/store';
 import { MoiTutorState } from '../store';
+import { LoginState } from '../reducers/login';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -25,11 +27,14 @@ export class LoginPage implements OnInit {
 
   errorMessage: string;
 
+  @select() login$: Observable<LoginState>;
+
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthenticateService,
     private navCtrl: NavController,
-    private ngRedux: NgRedux<MoiTutorState>
+    private ngRedux: NgRedux<MoiTutorState>,
+    public loadingController: LoadingController
   ) {
     this.loginForm = this.formBuilder.group({
       login: new FormControl('', Validators.compose([
@@ -39,22 +44,32 @@ export class LoginPage implements OnInit {
         Validators.required
       ]))
     });
+
+    // this.login$.subscribe((newState) => {
+    // });
+
   }
 
   ngOnInit() {
   }
 
-  loginUser(credentials) {
+  async loginUser(credentials) {
     const loginUser$ = this.authService.loginUser(credentials);
-
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+      spinner: 'bubbles'
+    });
+    await loading.present();
     loginUser$.subscribe({
-      next: (response) => {
+      next: async (response) => {
         this.errorMessage = '';
         this.ngRedux.dispatch({type: LOGIN});
         this.navCtrl.navigateForward('/home');
+        await loading.dismiss();
       },
-      error: (errMessage) => {
+      error: async (errMessage) => {
         this.errorMessage = errMessage;
+        await loading.dismiss();
       },
     });
   }
