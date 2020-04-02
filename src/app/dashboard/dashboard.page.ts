ba0de6fe97a7493d6ccab98cb1ebf1b1dd56efbb
Jhymer Martínez <jhmartinez1991@gmail.com>
2020-04-02 15:21:28 -0500

    Add tabs in the dashboard view

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
+  cards = [
+    {
+      key: 'clients',
+      icon: 'people-outline',
+    },
+    {
+      key: 'students',
+      icon: 'body-outline'
+    },
+    {
+      key: 'messages',
+      icon: 'chatbox-outline'
+    },
+    {
+      key: 'quizzes',
+      icon: 'clipboard-outline'
+    },
+    {
+      key: 'recommendations',
+      icon: 'help-buoy-outline'
+    },
+  ];
+
+  selectedTab = this.cards[0].key;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  segmentChanged(event) {
+    const value = event.detail.value;
+    this.selectedTab = value;
   }
 
 }
