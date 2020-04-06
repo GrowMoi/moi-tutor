dbb1054849bcb7c12e662bc81afa5fe62fe4ce68
Jhymer Martínez <jhmartinez1991@gmail.com>
2020-04-06 18:16:39 -0500

    Add missing tests in the intro page

--- src/app/intro/intro.page.spec.ts
+++ src/app/intro/intro.page.spec.ts
@@ -1,9 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
 
 import { IntroPage } from './intro.page';
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule, Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
+import { MockRouter } from '../../__mocks__/router.mock';
+import { MockStorage } from 'src/__mocks__/storage.mock';
 
 describe('IntroPage', () => {
   let component: IntroPage;
@@ -16,7 +20,14 @@ describe('IntroPage', () => {
         IonicStorageModule.forRoot(),
         RouterTestingModule,
         IonicModule.forRoot()
-      ]
+      ],
+      providers: [{
+        provide: Router,
+        useClass: MockRouter
+      }, {
+        provide: Storage,
+        useClass: MockStorage
+      }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(IntroPage);
@@ -27,4 +38,17 @@ describe('IntroPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('navigateByUrl should be called', inject([Router], (router: Router) => {
+    const spy = spyOn(router, 'navigateByUrl');
+    component.finish();
+    expect(spy.calls.first().args[0]).toBe('/home');
+  }));
+
+  it('should storage the intro showed value', inject([Storage], (storage: Storage) => {
+    const spy = spyOn(storage, 'set');
+    component.finish();
+    expect(spy.calls.first().args[0]).toBe(environment.IS_INTRO_SHOWED_KEY);
+    expect(spy.calls.first().args[1]).toBe(true);
+  }));
 });
--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
 import { environment } from 'src/environments/environment';
@@ -8,16 +8,18 @@ import { environment } from 'src/environments/environment';
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
 })
-export class IntroPage implements OnInit {
 
-  constructor(private router: Router, private storage: Storage) { }
+
+export class IntroPage {
+
+  constructor(
+    private router: Router,
+    private storage: Storage
+  ) {}
 
   finish() {
     this.storage.set(environment.IS_INTRO_SHOWED_KEY, true);
     this.router.navigateByUrl('/home');
-  }
-
-  ngOnInit() {
   }
 
 }
