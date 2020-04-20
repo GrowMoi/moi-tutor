c3f1a476a3520a5a569a94cec9c3aeba9dfd7d10
Jhymer Martínez <jhmartinez1991@gmail.com>
2020-04-19 23:56:09 -0500

    Update Recommendations Service

--- src/app/services/recommendations.service.ts
+++ src/app/services/recommendations.service.ts
@@ -4,7 +4,14 @@ import { ToastService } from './toast.service';
 import { UtilsService } from './utils.service';
 import { MoiTutorState } from '../store';
 import { NgRedux } from '@angular-redux/store';
-import { LOAD_ACHIEVEMENTS, LOAD_ACHIEVEMENTS_SUCCESS, LOAD_ACHIEVEMENTS_ERROR } from '../actions';
+import {
+  LOAD_ACHIEVEMENTS,
+  LOAD_ACHIEVEMENTS_SUCCESS,
+  LOAD_ACHIEVEMENTS_ERROR,
+  LOAD_CONTENTS,
+  LOAD_CONTENTS_SUCCESS,
+  LOAD_CONTENTS_ERROR
+} from '../actions';
 
 @Injectable({
   providedIn: 'root'
@@ -34,5 +41,34 @@ export class RecommendationsService {
         this.ngRedux.dispatch({ type: LOAD_ACHIEVEMENTS_ERROR });
         return Promise.reject(error);
       });
+  }
+
+  getContents(studentId?: number) {
+
+    const options: any = {
+      method: 'get',
+      url: '/tutor/dashboard/get_contents',
+    };
+    if (studentId) {
+      options.params = {
+        user_id: studentId
+      };
+    }
+    this.ngRedux.dispatch({ type: LOAD_CONTENTS });
+    return this.httpService.http(options)
+      .then((response) => {
+        const contents = response.data && response.data.data ? response.data.data : response.data;;
+        this.ngRedux.dispatch({ type: LOAD_CONTENTS_SUCCESS, payload: contents });
+        return response.data;
+      })
+      .catch((error) => {
+        const message = this.utilsService.getErrorMessage(error);
+        this.ngRedux.dispatch({ type: LOAD_CONTENTS_ERROR });
+        return Promise.reject(error);
+      });
+  }
+
+  cleanContents() {
+    // TODO
   }
 }
